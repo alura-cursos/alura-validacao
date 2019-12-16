c06f874a1fb9c88fff7c16d7faea65ec30fd2c8c
Samuel Monteiro <samuel.pinho@fatec.sp.gov.br>
2019-12-16 01:54:16 -0200

    commit

--- services/app.js
+++ services/app.js
@@ -4,19 +4,6 @@ window.onload = function () {
   const inputs = document.querySelectorAll('input');
 
   inputs.forEach(input => {
-    if (input.dataset.tipo === 'preco') {
-      SimpleMaskMoney.setMask(input, {
-        allowNegative: false,
-        negativeSignAfter: false,
-        prefix: 'R$ ',
-        fixed: true,
-        fractionDigits: 2,
-        decimalSeparator: ',',
-        thousandsSeparator: '.',
-        cursor: 'move'
-      });
-    }
-
     input.addEventListener('invalid', (e) => {
       e.preventDefault();
 
